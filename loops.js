d05ca212e8698f085155edd436d9ea9e07c33c24
Dru Edmondson <peryton6@gmail.com>
2018-07-09 14:37:59 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -4,7 +4,7 @@ for (var i = 0; i < 25; i++){
     console.log("I am 1 strange loop.")
   }
   else{
-    console.log("I am ${i} strange loops")
+    console.log(`I am ${i} strange loops`)
   }
 }
 }
