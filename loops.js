d0fd40ff1930271dc23f444ce1e7b787fd22d5de
Dru Edmondson <peryton6@gmail.com>
2018-07-09 15:10:22 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,10 +1,10 @@
 function forLoop(theArray){
 for (var i = 0; i < 25; i++){
   if (i === 1){
-    theArray.push("I am 1 strange loop.")
+    return theArray.push("I am 1 strange loop.")
   }
   else{
-    theArray.push(`I am ${i} strange loops`)
+    return theArray.push(`I am ${i} strange loops`)
   }
 }
 }
