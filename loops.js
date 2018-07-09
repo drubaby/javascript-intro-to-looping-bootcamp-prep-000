5f9ee6d313dc805cbf8e28472cbbfaa373f30cf2
Dru Edmondson <peryton6@gmail.com>
2018-07-09 16:15:02 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -23,7 +23,7 @@ function maybeTrue() {
 
 function doWhileLoop(theArray){
   do {
-    return theArray.shift()
+    return theArray.pop()
   }
  while (theArray.length > 0 && maybeTrue())
  return theArray
